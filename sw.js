aba0efa57ff2a03cec9d2624594a284f83efc3db
Xakute <chadb@duck.com>
2025-09-24 19:35:44 -0400

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE_NAME = 'dinner-log-cache-v9'; // Increment cache version
+const CACHE_NAME = 'dinner-log-cache-v10'; // Increment cache version
 const urlsToCache = [
   '.',
   'index.html',
@@ -45,4 +45,3 @@ self.addEventListener('fetch', event => {
     )
   );
 });
-
